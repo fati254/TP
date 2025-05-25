092caa8aabc4c16f2dcc681d77e456ab4e35d4c3
fati254 <fatifatimalka@gmail.com>
2025-05-25 11:35:34 +0100

    tp5.c

--- tp5.c
+++ tp5.c
@@ -1,3 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct Noeud {
+    int valeur;
+    struct Noeud* suivant;
+} Noeud;
+
+void push(Noeud** top, int valeur) {
+    Noeud* nouveau = (Noeud*)malloc(sizeof(Noeud));
+    nouveau->valeur = valeur;
+    nouveau->suivant = *top;
+    *top = nouveau;
+}
+
+void pop(Noeud** top) {
+    if (*top == NULL) {
+        printf("Pile vide.\n");
+        return;
+    }
+    Noeud* temp = *top;
+    *top = (*top)->suivant;
+    free(temp);
+}
+
+void peek(Noeud* top) {
+    if (top == NULL) {
+        printf("Pile vide.\n");
+    } else {
+        printf("Sommet de la pile : %d\n", top->valeur);
+    }
+}
+
+void afficher(Noeud* top) {
+    printf("Contenu de la pile : ");
+    while (top != NULL) {
+        printf("%d ", top->valeur);
+        top = top->suivant;
+    }
+    printf("\n");
+}
+
+int taille(Noeud* top) {
+    int count = 0;
+    while (top != NULL) {
+        count++;
+        top = top->suivant;
+    }
+    return count;
+}
+
+
 Noeud* debut = NULL;
 Noeud* fin = NULL;
 
@@ -5,7 +58,6 @@ void Enfiler(int valeur) {
     Noeud* nouveau = (Noeud*)malloc(sizeof(Noeud));
     nouveau->valeur = valeur;
     nouveau->suivant = NULL;
-
     if (fin == NULL) {
         debut = fin = nouveau;
     } else {
@@ -27,17 +79,4 @@ void Defiler() {
 }
 
 void afficherFile() {
-    printf("File : ");
-    Noeud* temp = debut;
-    while (temp != NULL) {
-        printf("%d ", temp->valeur);
-        temp = temp->suivant;
-    }
-    printf("\n");
-}
-
-void viderFile() {
-    while (debut != NULL) {
-        Defiler();
-    }
-}
+    Noeud
